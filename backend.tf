a50a711d1d65b17622e9ee644a6e866033a8d738
Dkflora <dkflora13@gmail.com>
2025-10-04 20:55:01 -0700

    Sanitize code: Add placeholder .tfvars and update .gitignore/backend.tf

--- backend.tf
+++ backend.tf
@@ -1,8 +1,8 @@
 terraform {
   backend "s3" {
-    bucket         = "makasiw7backet"
+    bucket         = "your-s3-bucket-name"
     key            = "terraform/state/dev/terraform.tfstate"  # Fixed for dev; change for other envs
     region         = "us-east-1"
-    dynamodb_table = "makasiw7backet-lock"
+    dynamodb_table = "your-s3-bucket-name-lock"
   }
 }
